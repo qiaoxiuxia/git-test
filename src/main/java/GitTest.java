3ea3635a8d23f6a262bfa4b61423fe96bcd49192
qiaoxiuxia <15680879850@163.com>
2023-08-26 09:51:27 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author qiaoxiuxia  15680879850@163.com
+ * @version 2023-08-26 9:40
+ * @since Java17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
